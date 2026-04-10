e1146288a198c96be77a6f4db316494ec2ac7b86
Andreas Hovaldt Hojrup <andreas.hovaldt@gmail.com>
2026-04-10 09:57:14 +0200

    Modified the pc export path

--- demo/0_pca_indoor.py
+++ demo/0_pca_indoor.py
@@ -116,15 +116,18 @@ if __name__ == "__main__":
     # point.inverse is acquired from the GirdSampling transform
     original_pca_color = pca_color[point.inverse]
     # Export original (RGB) point cloud
+    from pathlib import Path
+    save_dir = Path(__file__).parent / "exports"
+    save_dir.mkdir(exist_ok=True)
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(original_coord)
     pcd.colors = o3d.utility.Vector3dVector(original_color)
-    o3d.io.write_point_cloud("pc.ply", pcd)
+    o3d.io.write_point_cloud(save_dir / "pc.ply", pcd)
     print("Wrote pc.ply")
 
     # Export PCA-colored point cloud
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(original_coord)
     pcd.colors = o3d.utility.Vector3dVector(original_pca_color.cpu().detach().numpy())
-    o3d.io.write_point_cloud("pca.ply", pcd)
+    o3d.io.write_point_cloud(save_dir / "pca.ply", pcd)
     print("Wrote pca.ply")
